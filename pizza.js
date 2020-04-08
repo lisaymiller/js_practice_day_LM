e1ad04d7fad3bc729847b4ef0b318690f53e9bcc
Lisa Miller <lisayevonne@outlook.com>
2020-04-08 09:39:43 -0400

    piza exercise

---
+++ pizza.js
@@ -0,0 +1,9 @@
+let pizzaToppings = ['meat', 'cheese', 'veggies']
+
+function pizza (description) {
+	return 'You can get a pizza with ' + pizzaToppings[0] + ', ' + pizzaToppings[1] + ', or ' + pizzaToppings[2] + '.';
+
+}
+
+console.log(pizza());
+
